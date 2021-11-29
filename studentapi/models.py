6fa968517fec3bfc6700eb7f0029ea21dc6bd9c7
Aiswariya Shaji <aiswariyashaji123@gmail.com>
2021-11-29 17:00:14 +0530

    Admin Panel Created

--- studentapi/models.py
+++ studentapi/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    regno = models.IntegerField()
+    name = models.CharField(max_length=50)
+    standard = models.SmallIntegerField()
+    marks = models.IntegerField()
+    def __str__(self):
+        return str(self.regno) + ' : '+ " " + self.name
+
+class Subject(models.Model):
+    regnum = models.ForeignKey(Student,on_delete = models.CASCADE)
+    maths = models.IntegerField()
+    english = models.IntegerField()
+    science = models.IntegerField()
+    socialscience = models.IntegerField()
+    def __str__(self):
+        return str(self.regnum)
+

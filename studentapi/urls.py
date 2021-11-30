d9e843d580bda2fe4b0268d5d1158809fb48e3b6
Aiswariya Shaji <aiswariyashaji123@gmail.com>
2021-11-30 12:56:57 +0530

    Delete Student API Created

--- studentapi/urls.py
+++ studentapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateStudent
+from .views import CreateStudent,DeleteStudent
 
 urlpatterns = [
     path('createapi/',CreateStudent.as_view()),
+    path('deleteapi/<pk>',DeleteStudent.as_view()),
+
 ]
--- studentapi/views.py
+++ studentapi/views.py
@@ -4,6 +4,11 @@ from .serializers import StudentSerializer
 from .models import Student
 # Create your views here.
 
-class CreateStudent(generics.CreateAPIView):
+class CreateStudent(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+class DeleteStudent(generics.RetrieveDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
